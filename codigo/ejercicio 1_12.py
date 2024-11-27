9e3b0e6a17799865ad3cd9ee6f11ede410304114
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-11-27 15:57:23 -0600

    Update ejercicio 1_12.py

--- codigo/ejercicio 1_12.py
+++ codigo/ejercicio 1_12.py
@@ -1,5 +1,6 @@
 # Programa para mandar a mi hermanito a la tienda (Tablas de verdad)
 # Fecha 2024/10/18
-# Realizado por Fausto Alexander Ramírez Ramírez
+# Realizado por Alejandro Martinez Martinez}
+
 
  
