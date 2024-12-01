b219cce8de85d73246aca4d415e894cb08ba0549
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-12-01 17:41:13 -0600

    Update ejercicio_3.py

--- Codigos/Bloque_1/ejercicio_3.py
+++ Codigos/Bloque_1/ejercicio_3.py
@@ -1,4 +1,4 @@
-print("ejercicio 1.3: IMPRIMIENTO LITERALES")
+print("Ejercicio 3: Imprimiendo Literales")
 print("El numero de dias en una semana es: 7")
 print("El nombre de una figura de 3 lados es: Triangulo")
-print("250cm en Metros son: 2.5")
+print("250 cm en Metros son: 2.5")
