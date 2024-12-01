467ff1a4fce07cfb6d82bd04c372e52210e60047
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-12-01 17:56:36 -0600

    Update and rename ejercicio 1_7.py to ejercicio_7.py

---
+++ Codigos/Bloque_1/ejercicio_7.py
@@ -0,0 +1,9 @@
+# Programa 7
+# Fecha: 2024/10/15
+# Alejandro Martinez Martinez
+print("Calcula el area y el perimetro de un circulo ingresando el valor del radio por el usuario")
+radio = float(input("Ingresa el valor del radio: "))
+area = 3.1416 * radio ** 2
+perimetro = 3.1416 * 2 * radio
+print("El area del circulo es: "+str(area))
+print("El perimetro del circulo es: "+str(perimetro))
