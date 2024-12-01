086ae7381906a6a60151d0955ed717d23904bfee
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-12-01 17:43:52 -0600

    Update and rename ejercicio 1_6.py to ejercicio_6.py

--- Codigos/Bloque_1/ejercicio_6.py
+++ Codigos/Bloque_1/ejercicio_6.py
@@ -1,6 +1,6 @@
-print("Ejercicio 1_6 calcular el area de un triangulo")
-#Fecha: 2024/10/15
-#Elaborado por: Fausto Alexander Ramírez Ramírez
+#Fecha: 15/10/2024
+#Elaborado por: Alejandro Martinez Martinez
+print("Ejercicio 6 calcular el area de un triangulo")
 base = float(input("Cuanto mide la base: "))
 altura= float(input("Cuanto mide la altura: "))
 area = base * altura / 2
