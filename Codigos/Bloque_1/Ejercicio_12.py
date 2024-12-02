bad3d4f32e67120df60b2d13b146c400ad269820
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-12-01 19:30:26 -0600

    Update and rename ejercicio 1_12.py to Ejercicio_12.py

---
+++ Codigos/Bloque_1/Ejercicio_12.py
@@ -0,0 +1,16 @@
+#Programa 12
+#Fecha de elaboracion: 18/10/2024
+#Elaborado por: Alejandro Martinez Martinez
+print("Compuertas OR y AND")
+print("Compuertas or")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+print("\n")
+
+print("Compuertas and")
+print(False and False)
+print(True and False)
+print(True and True)
+print("\n")
