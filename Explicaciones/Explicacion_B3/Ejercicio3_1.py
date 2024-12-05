59647ed1a2f619762b1bbfb5aa53ac70561a86ee
AlejandroHpMel <L241050172@pabellon.tecnm.mx>
2024-12-05 13:48:18 -0600

    Add files via upload

---
+++ Explicaciones/Explicacion_B3/Ejercicio3_1.py
@@ -0,0 +1,15 @@
+#Programa.1 
+#Fecha de elaboracion: 28/10/2024
+#Elaborado por: Alejandro Martinez Martinez
+print("Elife")
+nombre = input("Ingresa el tipo de mascota que tienes:  ")
+if "perro" in nombre:
+    print ("Es un perro")
+
+elif "gato" in nombre:
+     print("Es un gato")
+
+
+else:
+     print("Mascota desconocida")
+
---
+++ Explicaciones/Explicacion_B3/Ejercicio3_10.py
@@ -0,0 +1,13 @@
+#Programa.10 
+#Fecha de elaboracion: 31/10/2024
+#Elaborado por: Alejandro Martinez Martinez
+print("Lista de membership")
+nombre=["Choto", "Emiliano", "Luis"]
+
+print("Luis" in nombre) #Identifica y evalua a TRUE
+
+print("Emi" in nombre) #Identifica y evalua a FALSE
+
+print("Javier" in nombre) #Identifica y evalua a FALSE
+
+print("Jose" in nombre) #Identifica y evalua a FALSE
